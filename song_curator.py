2e7535ac1681494ba788e831c8e2a168626d8149
Moses2917 <moses2917@gmail.com>
2024-09-15 01:58:53 -0700

    Update song_curator.py

--- song_curator.py
+++ song_curator.py
@@ -13,7 +13,8 @@ from random import choice as choose
 from random import choices
 from scanningDir import songCollector
 # attr_finder.attrFinder.attributeSearch()
-
+print("Bugging A.N.I. Quyr...")
+print("Awakening A.N.I. Quyr...\nAnalyzing, Narrowing, and Identifying the perfect worship setlist...")
 sang_in_last_3months = songCollector(ignore_sundays=True)
 sang_in_last_year = songCollector(three_month_window=False, search_range=360, ignore_sundays=True)
 
@@ -40,6 +41,27 @@ for key in sang_in_last_year:
 opening_songs: list = choose(possible_opening_songs)
 # print(possible_residual_songs, possible_opening_songs[-1])
 latter_half_songs: list = choices(possible_latter_half_songs, k=2)
-possible_latter_half_songs.remove(latter_half_songs[0]).remove(latter_half_songs[1])
+possible_latter_half_songs.remove(latter_half_songs[0])#.remove(latter_half_songs[1])
 latter_half_songs.append(choices(possible_latter_half_songs, k=2))
 print(f"This is today's song order:\n{opening_songs}{latter_half_songs}")
+
+
+def find_song(only_first_two_songs=False, only_worship_songs=False, only_last_two_songs=False):
+    # Only for sunday songs
+    if only_first_two_songs:
+        return opening_songs
+    elif only_worship_songs:
+        return latter_half_songs[0]
+    elif only_last_two_songs:
+        return latter_half_songs[1]
+    else:
+        possible_sunday_redos = []
+        sang_in_last_3months = songCollector(sunday_only=True)
+        sang_in_last_year = songCollector(sunday_only=True,three_month_window=False, search_range=360)
+        for key in sang_in_last_year:
+            if not sang_in_last_3months.get(key, None):
+                possible_sunday_redos.append(literal_eval(sang_in_last_year[key]["songList"]))
+
+        return choose(possible_sunday_redos)
+
+print(f"Haven't sang this in a while!\n{find_song()}")
