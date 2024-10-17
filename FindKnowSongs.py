4215144cb3726c56eca9b1feb53993e0e1fdbb9d
ArmPentChurch <armnewchurch@gmail.com>
2024-10-16 18:56:44 -0700

    Update FindKnowSongs.py

--- FindKnowSongs.py
+++ FindKnowSongs.py
@@ -3,6 +3,7 @@
 #     future = exec.submit(openWord,songnum,book)
 #     lyrics = future.result()
 
+
 def getSong(book:str, songnum:str, batch = 0) -> dict:
     """
     Retrieves a song from a JSON file based on the provided book and song number. The options are 'old', 'new', 'redergaran', and 'wordsongsindex'.
@@ -28,10 +29,31 @@ def getSong(book:str, songnum:str, batch = 0) -> dict:
     else:
         pass
 
-with open('songs_cleaned.json', 'r', encoding='utf-8') as f:
-    from json import load
-    allSongs: dict = load(f)
+def isSongInAllSongs(songnum:str|int, full_songList:dict, book = 'New') -> bool:
+    if full_songList.get((book, songnum), None):
+        return True
+    return False
 
-full_songList = []
-for date in allSongs:
-    songlist = eval(allSongs[date]["songList"])
+def getSongs() -> dict:
+    with open('songs_cleaned.json', 'r', encoding='utf-8') as f:
+        from json import load
+        allSongs: dict = load(f)
+
+    full_songList = {}
+    for date in allSongs:
+        songlist = eval(allSongs[date]["songList"])
+        for song in songlist:
+            if not (full_songList.get(song, None)):
+                full_songList[song] = True
+    return full_songList #Can choose to save or just to continue coding
+
+def checkAllIfSang() -> dict:
+    with open("REDergaran.json", 'r', encoding='utf-8') as redSongs:
+        from json import load
+        redSongs: dict = load(redSongs)
+    notSang = []
+    full_songList = getSongs()
+    for songnum in redSongs["SongNum"]: #Could make groups of like 2-5 and do multithreading
+        if not(isSongInAllSongs(songnum,full_songList)):
+            notSang.append(songnum)
+    return notSang
