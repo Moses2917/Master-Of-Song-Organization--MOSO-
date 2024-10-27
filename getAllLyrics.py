0370998e4475d8e24d1bc784c76ce78fdf72ad1d
ArmPentChurch <armnewchurch@gmail.com>
2024-10-27 11:21:52 -0700

    Update getAllLyrics.py

--- getAllLyrics.py
+++ getAllLyrics.py
@@ -5,16 +5,21 @@ from docx import Document
 basePth = ENV.get("OneDrive")
 #open all files, get latest version and store lyrics in dict
 # if songLyrics.get('latestChange',None) or do < today()
-songLyrics = {
-    'old': {},
-    'new': {},
-    'latestChange': today()
-}
-songs_to_open = {
-    'old': {},
-    'new': {}
-}
-def GetAllSongPths(index:str, book = 'old'or'new') -> dict:
+
+#templates for songLyrics
+def template(songs_to_open_bool=True) -> dict:
+    songLyrics = {
+        'old': {},
+        'new': {},
+        'latestChange': today()
+    }
+    songs_to_open = {
+        'old': {},
+        'new': {}
+    }
+    return songs_to_open if songs_to_open_bool else songLyrics
+
+def GetAllSongPths(songs_to_open,index:str, book = 'old'or'new',) -> dict:
     try:
         for songNum in index["SongNum"]:
             songPth_realative = index["SongNum"][songNum]['latestVersion']
@@ -32,22 +37,28 @@ def readLyrics(filePth:str) -> str:
     if lyrics != '':
         return lyrics
     return None
-#Start from olds
-with open('wordSongsIndex.json', 'r', encoding='utf-8') as f:
-    oldSongs = load(f)
-    GetAllSongPths(oldSongs,'old')
-# Close file and continue with reading all red songs
-with open('REDergaran.json', 'r', encoding='utf-8') as f:
-    newSongs = load(f)
-    GetAllSongPths(newSongs,'new')
-# with open('test.json', 'w', encoding='utf-8') as f:
-#     from json import dump
-#     dump(songs_to_open,f,ensure_ascii=False,indent=4)
-for book in songs_to_open:
-    for songNum in songs_to_open[book]:
-        filePth = songs_to_open[book][songNum]
-        lyrics = readLyrics(filePth)
-        songLyrics[book][songNum] = lyrics
-with open('AllLyrics.json', 'w', encoding='utf-8') as f:
-    from json import dump
-    dump(songLyrics,f,ensure_ascii=False,indent=4)
+
+def updateLyrics(book:str, songNum:str):
+    return None
+def getAllLyrics():
+    songs_to_open = template()
+    songLyrics = template(songs_to_open_bool=False)
+    #Start from olds
+    with open('wordSongsIndex.json', 'r', encoding='utf-8') as f:
+        oldSongs = load(f)
+        GetAllSongPths(oldSongs,'old')
+    # Close file and continue with reading all red songs
+    with open('REDergaran.json', 'r', encoding='utf-8') as f:
+        newSongs = load(f)
+        GetAllSongPths(newSongs,'new')
+    # with open('test.json', 'w', encoding='utf-8') as f:
+    #     from json import dump
+    #     dump(songs_to_open,f,ensure_ascii=False,indent=4)
+    for book in songs_to_open:
+        for songNum in songs_to_open[book]:
+            filePth = songs_to_open[book][songNum]
+            lyrics = readLyrics(filePth)
+            songLyrics[book][songNum] = lyrics
+    with open('AllLyrics.json', 'w', encoding='utf-8') as f:
+        from json import dump
+        dump(songLyrics,f,ensure_ascii=False,indent=4)
